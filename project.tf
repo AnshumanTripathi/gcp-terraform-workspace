06626a5ec0b44c8a109b037d03643d290a13e169
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2023-02-26 00:18:25 -0800

    Fix service account
    Signed-off-by: Anshuman Tripathi <anshuman.tripathi305@gmail.com>

--- project.tf
+++ project.tf
@@ -17,12 +17,13 @@ resource "google_service_account" "terraform_former" {
   account_id   = "terraform-former"
   project      = local.google_project
   display_name = "terraform-former"
-  description = "Service account for terraform"
+  description  = "Service account for terraform"
 }
 
-
+# Non authoritative policy
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account_iam#google_service_account_iam_member
 resource "google_service_account_iam_member" "terraform_former_iam_policy" {
-  service_account_id = google_service_account.terraform_former.id
-  member             = "serviceAccount:${google_service_account.terraform_former.name}"
+  service_account_id = google_service_account.terraform_former.name
+  member             = "serviceAccount:${google_service_account.terraform_former.email}"
   role               = "roles/owner"
 }
