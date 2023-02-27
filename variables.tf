9c32ca58541bd981da9b91b546a160e0a4eb7681
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2023-02-27 00:12:33 -0800

    Enable cloudresourcemanager.googleapis.com
    Signed-off-by: Anshuman Tripathi <anshuman.tripathi305@gmail.com>

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 locals {
   google_project  = "anshumantripathi-samyak"
   billing_project = "014B18-76E511-9E2113"
-  terraform_image = "hashicorp/terraform:1.3.9"
   services = [
+    "cloudresourcemanager.googleapis.com",
     "iam.googleapis.com",
     "compute.googleapis.com",
   ]
