c23615f16665ac68a8fa65e8f2f0b79d211301a8
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2023-06-03 17:22:55 -0700

    Setup VPC
    Signed-off-by: Anshuman Tripathi <anshuman.tripathi305@gmail.com>

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,7 @@
-#resource "google_compute_network" "vpc_network" {
-#  project                 = local.google_project
-#  name                    = "${local.google_project}-vpc-network"
-#  auto_create_subnetworks = true
-#}
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
+resource "google_compute_network" "vpc_network" {
+  project                 = local.google_project
+  name                    = "${local.google_project}-vpc-network"
+  routing_mode            = "GLOBAL"
+  auto_create_subnetworks = false
+}
