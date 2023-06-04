a9a9a869c7b5f7541547f4dfcddff6e0dc126053
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2023-06-03 18:14:34 -0700

    Fix subnets
    Signed-off-by: Anshuman Tripathi <anshuman.tripathi305@gmail.com>

--- vpc.tf
+++ vpc.tf
@@ -9,21 +9,21 @@ resource "google_compute_network" "vpc_network" {
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_subnetwork
 resource "google_compute_subnetwork" "kubernetes_subnet" {
   name                     = "kubernetes-subnet"
-  ip_cidr_range            = "10.0.0.0/19"
+  ip_cidr_range            = "10.0.0.0/20"
   region                   = local.environment_config.region
   network                  = google_compute_network.vpc_network.id
   project                  = local.environment_config.google_project
   private_ip_google_access = true
   secondary_ip_range {
     range_name    = "pods"
-    ip_cidr_range = "10.0.0.0/21"
+    ip_cidr_range = "10.0.16.0/21"
   }
   secondary_ip_range {
     range_name    = "services"
-    ip_cidr_range = "10.0.8.0/22"
+    ip_cidr_range = "10.0.24.0/22"
   }
   secondary_ip_range {
     range_name    = "control-plane"
-    ip_cidr_range = "10.0.12.0/28"
+    ip_cidr_range = "10.0.28.0/28"
   }
 }
